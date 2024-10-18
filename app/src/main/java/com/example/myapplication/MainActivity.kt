9b53e990917447b04096ae23eec81e3b2cd3a8b9
Riley Ho <riley@rileychh.dev>
2024-10-18 16:52:33 +0800

    refactor: replace findViewById with view binding
    https://developer.android.com/topic/libraries/view-binding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.*
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
@@ -21,55 +21,48 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Connect XML components
-        val editTextName = findViewById<EditText>(R.id.editTextName)
-        val textViewStatus = findViewById<TextView>(R.id.textViewStatus)
-        val radioButtonScissors = findViewById<RadioButton>(R.id.radioButtonScissors)
-        val radioButtonRock = findViewById<RadioButton>(R.id.radioButtonRock)
-        val radioButtonPaper = findViewById<RadioButton>(R.id.radioButtonPaper)
-        val buttonPlay = findViewById<Button>(R.id.buttonPlay)
-        val textViewName = findViewById<TextView>(R.id.textViewName)
-        val textViewWinner = findViewById<TextView>(R.id.textViewWinner)
-        val textViewPlayerPlayed = findViewById<TextView>(R.id.textViewPlayerPlayed)
-        val textViewComputerPlayed = findViewById<TextView>(R.id.textViewComputerPlayed)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
 
-        buttonPlay.setOnClickListener { _ ->
-            if (editTextName.length() < 1) {
-                textViewStatus.text = "請輸入玩家姓名"
+        binding.buttonPlay.setOnClickListener { _ ->
+            if (binding.editTextName.length() < 1) {
+                binding.textViewStatus.text = "請輸入玩家姓名"
                 return@setOnClickListener
             }
 
-            textViewName.text = "名字\n" + editTextName.text
+            binding.textViewName.text = "名字\n$binding.editTextName.text"
 
-            if (radioButtonScissors.isChecked) {
-                textViewPlayerPlayed.text = "我方出拳\n剪刀"
-            } else if (radioButtonRock.isChecked) {
-                textViewPlayerPlayed.text = "我方出拳\n石頭"
+            if (binding.radioButtonScissors.isChecked) {
+                binding.textViewPlayerPlayed.text = "我方出拳\n剪刀"
+            } else if (binding.radioButtonRock.isChecked) {
+                binding.textViewPlayerPlayed.text = "我方出拳\n石頭"
             } else {
-                textViewPlayerPlayed.text = "我方出拳\n布"
+                binding.textViewPlayerPlayed.text = "我方出拳\n布"
             }
 
             val computerRandom = (Math.random() * 3).toInt()
             if (computerRandom == 0) {
-                textViewComputerPlayed.text = "電腦出拳\n剪刀"
+                binding.textViewComputerPlayed.text = "電腦出拳\n剪刀"
             } else if (computerRandom == 1) {
-                textViewComputerPlayed.text = "電腦出拳\n石頭"
+                binding.textViewComputerPlayed.text = "電腦出拳\n石頭"
             } else {
-                textViewComputerPlayed.text = "電腦出拳\n布"
+                binding.textViewComputerPlayed.text = "電腦出拳\n布"
             }
 
-            if ((radioButtonScissors.isChecked && computerRandom == 2) ||
-                (radioButtonRock.isChecked && computerRandom == 0) ||
-                (radioButtonPaper.isChecked && computerRandom == 1)) {
-                textViewWinner.text = "勝利者\n" + editTextName.text
-                textViewStatus.text = "恭喜您獲勝了！！！"
-            } else if ((radioButtonScissors.isChecked && computerRandom == 1) ||
-                (radioButtonRock.isChecked && computerRandom == 2) ||
-                (radioButtonPaper.isChecked && computerRandom == 0)) {
-                textViewWinner.text = "勝利者\n電腦"
-                textViewStatus.text = "可惜，電腦獲勝了！"
+            if ((binding.radioButtonScissors.isChecked && computerRandom == 2) ||
+                (binding.radioButtonRock.isChecked && computerRandom == 0) ||
+                (binding.radioButtonPaper.isChecked && computerRandom == 1)
+            ) {
+                binding.textViewWinner.text = "勝利者\n$binding.editTextName.text"
+                binding.textViewStatus.text = "恭喜您獲勝了！！！"
+            } else if ((binding.radioButtonScissors.isChecked && computerRandom == 1) ||
+                (binding.radioButtonRock.isChecked && computerRandom == 2) ||
+                (binding.radioButtonPaper.isChecked && computerRandom == 0)
+            ) {
+                binding.textViewWinner.text = "勝利者\n電腦"
+                binding.textViewStatus.text = "可惜，電腦獲勝了！"
             } else {
-                textViewWinner.text = "勝利者\n平手"
-                textViewStatus.text = "平局，請再試一次！"
+                binding.textViewWinner.text = "勝利者\n平手"
+                binding.textViewStatus.text = "平局，請再試一次！"
             }
         }
 
