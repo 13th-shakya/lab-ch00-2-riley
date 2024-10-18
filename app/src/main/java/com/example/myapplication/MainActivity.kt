7a42803f7b00d7c313373a2b99cb2abc764dbff0
Riley Ho <riley@rileychh.dev>
2024-10-18 15:37:59 +0800

    chore: remove unused imports

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
